e8fd58dc042eb03dbbbf20fd6fbc033071ee2917
jonheslop <jon.heslop@pusher.com>
2020-11-03 13:27:58 +0000

    Refactor to use for loop
    Clean it up a bit
    
    With coaching and guidance from Callum
    
    Co-authored-by: Callum Oakley <hello@callumoakley.net>

--- _data/contentful-tutorial.js
+++ _data/contentful-tutorial.js
@@ -7,33 +7,35 @@ const client = contentful.createClient({
 // Contentful gives 100 entries at a time, we have more than that so have to get all of them
 
 module.exports = async () => {
-  // find out how many we have in total
+  // Find out how many we have in total
   const { total } = await client.getEntries({
     content_type: "post",
     limit: 1,
   });
 
-  // Make x requests to Contentful for each batch of entries
-  const arrayOfGetEntriesPromises = [...Array(Math.ceil(total / 100))].map(
-    async (_, i) => {
+  // Make array getEntries async functions
+  let pendingGetEntries = [];
+  for (let fetched = 0; fetched < total; fetched += 100) {
+    pendingGetEntries.push(async () => {
       const entries = await client.getEntries({
         content_type: "post",
         order: "sys.createdAt",
-        skip: 100 * i,
+        skip: fetched,
       });
 
       return entries.items.map((tutorials) => {
         tutorials.fields.date = new Date(tutorials.sys.updatedAt);
         return tutorials.fields;
       });
-    }
-  );
+    });
+  }
 
-  const arrayOfEntriesBatches = await Promise.all(arrayOfGetEntriesPromises);
+  // Run each getEntries function and wait for all to resolve
+  const completedEntriesBatches = await Promise.all(
+    pendingGetEntries.map((f) => f())
+  );
 
   // Transform [[{...}, {...}], [{...}, {...}]]
   // to [{...}, {...}, {...}, {...}]
-  const allTutorials = arrayOfEntriesBatches.reduce((a, b) => a.concat(b), []);
-
-  return allTutorials;
+  return completedEntriesBatches.reduce((a, b) => a.concat(b), []);
 };
